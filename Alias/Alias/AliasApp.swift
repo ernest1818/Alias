1764e0ba761ea6765f95a6fbf310c16e39ad2416
Ernest Avagovich <erni1937@gmail.com>
2025-01-16 23:22:59 +0300

    init

---
+++ Alias/Alias/AliasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasApp.swift
+//  Alias
+//
+//  Created by Ernest Avagovich on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AliasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
